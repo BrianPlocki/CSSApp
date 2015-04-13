4022dd7d4058e90724eb1ab9c54c5e5f7f4978f1
BrianPlocki <bplocki@uw.edu>
2015-04-13 12:02:37 -0700

    Added fields, getters, and setters to Course class.

--- app/src/main/java/com/sample/cssapp/Course.java
+++ app/src/main/java/com/sample/cssapp/Course.java
@@ -4,4 +4,49 @@ package com.sample.cssapp;
  * Created by Brandon on 4/13/2015.
  */
 public class Course {
+
+    private String mCourseId;
+    private String mShortCourseDescription;
+    private String mLongCourseDescription;
+    private String mPrerequisites;
+
+    public Course(String courseId,String shortCourseDescription,String longCourseDescription,String prerequisites) {
+
+        mCourseId = courseId;
+        mShortCourseDescription = shortCourseDescription;
+        mLongCourseDescription = longCourseDescription;
+        mPrerequisites = prerequisites;
+    }
+
+    public String getmCourseId() {
+        return mCourseId;
+    }
+
+    public void setmCourseId(String mCourseId) {
+        this.mCourseId = mCourseId;
+    }
+
+    public String getmShortCourseDescription() {
+        return mShortCourseDescription;
+    }
+
+    public void setmShortCourseDescription(String mShortCourseDescription) {
+        this.mShortCourseDescription = mShortCourseDescription;
+    }
+
+    public String getmLongCourseDescription() {
+        return mLongCourseDescription;
+    }
+
+    public void setmLongCourseDescription(String mLongCourseDescription) {
+        this.mLongCourseDescription = mLongCourseDescription;
+    }
+
+    public String getmPrerequisites() {
+        return mPrerequisites;
+    }
+
+    public void setmPrerequisites(String mPrerequisites) {
+        this.mPrerequisites = mPrerequisites;
+    }
 }
